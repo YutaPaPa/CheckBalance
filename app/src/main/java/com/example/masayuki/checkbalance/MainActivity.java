fb884fde1f85cf5e099445ff88d0da0299145d93
Masayuki <49ers.Champ@gmail.com>
2017-07-19 01:09:40 +0900

    ノートPCで修正

--- app/src/main/java/com/example/masayuki/checkbalance/MainActivity.java
+++ app/src/main/java/com/example/masayuki/checkbalance/MainActivity.java
@@ -2,7 +2,8 @@ package com.example.masayuki.checkbalance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageButton;
+import android.util.Log;
+import android.widget.Button;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -12,39 +13,50 @@ import android.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText editTextEdy;
+    private Button btnEdy;
+    private SharedPreferences CheckBalance;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton btnSendEdy = (ImageButton) this.findViewById(R.id.imageButton1);
-        btnSendEdy.setOnClickListener(this);
 
-        int edyMoney = this.loadEdy(this);
-        EditText editTextEdy = (EditText) this.findViewById(R.id.editTextEdy);
-        editTextEdy.setText(edyMoney);
+        // コントロールのセット
+        findViews();
+
+        // Edy残額のセット
+        int edyMoney = 0;
+        edyMoney = loadEdy();
+        editTextEdy.setText(String.valueOf(edyMoney));
+
+        // 登録ボタンにClickイベント設定
+        btnEdy.setOnClickListener(this);
+    }
+
+    protected void findViews(){
+        editTextEdy = (EditText)findViewById(R.id.editTextEdy);
+        btnEdy = (Button)findViewById(R.id.button);
     }
 
     @Override
     public void onClick(View v) {
-        EditText editTextEdy = (EditText) this.findViewById(R.id.editTextEdy);
-        editTextEdy.requestFocus();
-
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.showSoftInput(editTextEdy, InputMethodManager.SHOW_FORCED);
+
+        int edy = Integer.parseInt(editTextEdy.getText().toString());
+        this.saveEdy(edy);
     }
 
     /**
      * Preferences にパーソナルデータを保存する
-     * @param context
      * @param money Edyの残額
      */
-    private void saveEdy(Context context, int money){
+    private void saveEdy(int money){
         // アプリ標準の Preferences を取得する
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-
+        SharedPreferences dataStore = getSharedPreferences("CheckBalance", MODE_PRIVATE);
         // Preferences に書き込むための Editor クラスを取得する
-        SharedPreferences.Editor editor = sp.edit();
-
+        SharedPreferences.Editor editor = dataStore.edit();
         // putXxxx("キー",データ) にて書き込むデータを登録する
         editor.putInt("edy", money);
 
@@ -53,22 +65,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     /**
-     * Preferences から年齢を読み出す
-     * @param context
+     * Preferences からEdy残額を読み出す
      * @return Edyの残額(なければ 0 を返す)
      */
-    private int loadEdy(Context context){
+    private int loadEdy(){
+        /*Log.d("ここまできている", "1");
         // アプリ標準の Preferences を取得する
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-        return sp.getInt("edy", 0);
-    }
+        Log.d("ここまできている", "2");*/
 
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        EditText editTextEdy = (EditText) this.findViewById(R.id.editTextEdy);
-        int edy = Integer.parseInt(editTextEdy.getText().toString());
-        this.saveEdy(this, edy);
+        // "DataStore"という名前でインスタンスを生成
+        CheckBalance = getSharedPreferences("CheckBalance", MODE_PRIVATE);
+        return CheckBalance.getInt("edy", 0);
     }
 }
