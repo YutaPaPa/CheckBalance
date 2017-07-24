e3e07e13380b4ea411e1a6ed72dde7d4206dd85f
YutaPaPa <49ers.Champ@gmail.com>
2017-07-25 00:15:21 +0900

    修正中・・・

--- app/src/main/java/com/example/masayuki/checkbalance/MainActivity.java
+++ app/src/main/java/com/example/masayuki/checkbalance/MainActivity.java
@@ -9,13 +9,26 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import java.text.NumberFormat;
+import android.app.AlertDialog;
+import android.content.res.Resources;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editTextEdy;
+    private EditText editTextSuica;
+    private EditText editTextPasumo;
+    private EditText editTextBunkadou;
     private Button btnEdy;
     private SharedPreferences CheckBalance;
+    private Resources res = getResources();
+    private EditText editTextDate;
+    private String cardNameArray[] = new String[4];
+    private int moneyArray[] = new int[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +38,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // コントロールのセット
         findViews();
 
+        // 登録日のセット
+        editTextDate.setKeyListener(null);  // 入力不可設定
+        Date date1 = new Date();
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy'年'MM'月'dd'日'");
+        editTextDate.setText("登録日付：" + sdf1.format(date1));
+
         // Edy残額のセット
-        int edyMoney = 0;
-        edyMoney = loadEdy();
-        editTextEdy.setText(String.valueOf(edyMoney));
+/*        int edyMoney = 0;
+        edyMoney = loadEdy();*/
+
+        // 各ICカードごとの残額をセット
+        Map<String, String> map = new HashMap<String, String>();
+        map = load();
+
+        // 「\」記号をつけて表示する
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+        for (String str : map.keySet()) {
+            switch (str)
+            {
+                case "edy":
+                    editTextEdy.setText(currencyFormat.format(map.get(str)));
+                    break;
+                case "suica":
+                    editTextSuica.setText(currencyFormat.format(map.get(str)));
+                    break;
+                case "pasumo":
+                    editTextPasumo.setText(currencyFormat.format(map.get(str)));
+                    break;
+                case "bunkadou":
+                    editTextBunkadou.setText(currencyFormat.format(map.get(str)));
+                    break;
+            }
+            //System.out.println(str + ":" + map.get(str));
+            // 結果：エアコン:air conditioner、パソコン:personal computer、リモコン:remote control
+        }
+
+/*        // 「\」記号をつけて表示する
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+        editTextEdy.setText(currencyFormat.format(edyMoney));*/
 
         // 登録ボタンにClickイベント設定
         btnEdy.setOnClickListener(this);
@@ -37,15 +85,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void findViews(){
         editTextEdy = (EditText)findViewById(R.id.editTextEdy);
         btnEdy = (Button)findViewById(R.id.button);
+        editTextDate = (EditText)findViewById(R.id.editTextDate);
     }
 
     @Override
     public void onClick(View v) {
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.showSoftInput(editTextEdy, InputMethodManager.SHOW_FORCED);
+        inputMethodManager.showSoftInput(editTextSuica, InputMethodManager.SHOW_FORCED);
+        inputMethodManager.showSoftInput(editTextPasumo, InputMethodManager.SHOW_FORCED);
+        inputMethodManager.showSoftInput(editTextBunkadou, InputMethodManager.SHOW_FORCED);
 
-        int edy = Integer.parseInt(editTextEdy.getText().toString());
-        this.saveEdy(edy);
+        moneyArray[0] = Integer.parseInt(editTextEdy.getText().toString());
+        moneyArray[1] = Integer.parseInt(editTextSuica.getText().toString());
+        moneyArray[2] = Integer.parseInt(editTextPasumo.getText().toString());
+        moneyArray[3] = Integer.parseInt(editTextBunkadou.getText().toString());
+
+        String cardNameArray[] = {"edy", "suica", "pasumo", "bunkadou"};
+
+        //this.saveEdy(edy);
+        save(cardNameArray, moneyArray);
     }
 
     /**
@@ -62,6 +121,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // 書き込みを確定する
         editor.commit();
+
+        // 登録完了メッセージ表示
+        finishMessage();
     }
 
     /**
@@ -77,5 +139,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // "DataStore"という名前でインスタンスを生成
         CheckBalance = getSharedPreferences("CheckBalance", MODE_PRIVATE);
         return CheckBalance.getInt("edy", 0);
+    }
+
+    /**
+     * Preferences にパーソナルデータを保存する
+     * @param cardNameArray カード名
+     * @param moneyArray 残額
+     */
+    private void save(String[] cardNameArray, int[] moneyArray){
+        // アプリ標準の Preferences を取得する
+        SharedPreferences dataStore = getSharedPreferences("CheckBalance", MODE_PRIVATE);
+        // Preferences に書き込むための Editor クラスを取得する
+        SharedPreferences.Editor editor = dataStore.edit();
+
+        // Edy残額
+        editor.putInt(cardNameArray[0], moneyArray[0]);
+        // Suica残額
+        editor.putInt(cardNameArray[1], moneyArray[1]);
+        // Pasumo残額
+        editor.putInt(cardNameArray[2], moneyArray[2]);
+        // 文化堂残額
+        editor.putInt(cardNameArray[3], moneyArray[3]);
+
+/*        for (int cnt =0; cnt <= cardNameArray.length; cnt++) {
+            // putXxxx("キー",データ) にて書き込むデータを登録する
+            editor.putInt(cardNameArray[cnt], moneyArray[cnt]);
+
+            // 書き込みを確定する
+            editor.commit();
+        }*/
+
+        // 書き込みを確定する
+        editor.commit();
+
+        // 登録完了メッセージ表示
+        finishMessage();
+    }
+
+    /**
+     * Preferences から各ICカードごとの残額を読み出す
+     */
+    private Map<String, String> load(){
+        /*Log.d("ここまできている", "1");
+        // アプリ標準の Preferences を取得する
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        Log.d("ここまできている", "2");*/
+
+        // "DataStore"という名前でインスタンスを生成
+        CheckBalance = getSharedPreferences("CheckBalance", MODE_PRIVATE);
+        final String edy = String.valueOf(CheckBalance.getInt("suica", 0));
+        final String suica = String.valueOf(CheckBalance.getInt("suica", 0));
+        final String pasumo = String.valueOf(CheckBalance.getInt("pasumo", 0));
+        final String bunkadou = String.valueOf(CheckBalance.getInt("bunkadou", 0));
+
+        Map<String, String> map = new HashMap<String, String>() {
+            {
+                put("edy", edy);
+                put("suica", suica);
+                put("pasumo", pasumo);
+                put("bunkadou", bunkadou);
+            }
+        };
+
+        return map;
+    }
+
+    private void finishMessage()
+    {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle(res.getString(R.string.finish_message_title).toString());
+        alert.setMessage(res.getString(R.string.finish_message).toString());
+        alert.setIcon(android.R.drawable.ic_dialog_info);
+        alert.setPositiveButton("OK", null);
+        alert.show();
     }
 }
